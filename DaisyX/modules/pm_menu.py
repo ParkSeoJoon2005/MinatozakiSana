9fe8fee6a48db5df07de29ba1d849cfadb0fdb53
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-11-20 12:58:37 +0530

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -73,7 +73,7 @@ async def start_group_cmd(message, strings):
     await message.reply(strings["start_hi_group"])
 
 
-@register(cmds="start", no_args=True, only_pm=true)
+@register(cmds="start", no_args=True, only_pm=True)
 async def start_cmd(message):
     await message.reply_sticker(random.choice(STICKERS))
     await get_start_func(message)
